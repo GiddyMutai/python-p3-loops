747c0ef9871af8ec5d76945ebd8c8ac156c2b025
Gideon Mutai <giddymutai13@gmail.com>
2024-05-27 23:33:29 +0300

    Finished task

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 11
+    while i != 1:
+        i -= 1
+        print(i)
+    else:
+        print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [number ** 2 for number in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i%3 == 0 and i%5 == 0:
+            print ("FizzBuzz")
+        elif i%3 == 0:
+            print ("Fizz")
+        elif i%5 == 0:
+            print ("Buzz")
+        else:
+            print(i)
